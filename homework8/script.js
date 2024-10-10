b5234f930e78d6110cd1d5187ff717b662df7759
Nino Kharazishvili <ninokharazishvili@credo.ge>
2024-10-10 17:49:30 +0400

    added hm8

---
+++ homework8/script.js
@@ -0,0 +1,16 @@
+// 1) Check if a string starts with an uppercase letter.
+
+// 2) Test if a string is a valid date in DD/MM/YYYY format
+
+// 3) Validate a GE phone number in the format 598-12-34-56
+
+// 4) Validate the emails that ends with @example.com
+
+// 5) Save the random horoscop data like 10 into localstorage and when user enter the website,
+// display different horoscop to difference day. like  first day first horoscop, second day second horoscop and etc.
+
+// 6) Make a form with three inputs name, email and phone number, when user try to enter each of this field you
+//should save this info into localstorage. if you typing info and refresh the page, the info that you wrote should not be deleted.
+
+// 7) Create a two button En Ka and the random text below, if you choose, en the random text should be translated
+//into english, when you click ka it should be translated into georgian language. use localstorage to save this info.
