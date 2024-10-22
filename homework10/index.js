de4e3f4dec4a01006512c0b59b956c034ec5829f
Nino Kharazishvili <ninokharazishvili@credo.ge>
2024-10-22 17:36:54 +0400

    added hm10

---
+++ homework10/index.js
@@ -0,0 +1,24 @@
+// 1) Write a function that receives 3 parameters: amount of money,
+// dayLimit and weekLimit you should calculate how many days does it needs to withdway whole
+// amount of money.
+
+const calcDays = (money, dayLimit, weekLimit) => {
+    
+};
+
+// 2) Write a function that takes text as a parameter, the text should be: "What is a plus b?"
+// or "What is a minus b?" you should write correct answer, if answer is correct console you're humar
+// other wise consoled you're robot
+
+// 3) write a function that takes 2 parameter the height and width you draw that rectangle with #
+// eg: 2, 2 =>
+// ##
+// ##
+
+// eg: 3:4
+// ####
+// ####
+// ####
+
+// 4) write a function that takes number as a parameter and check is this number wide or not.
+// * wide means that If the number of its digits is greater than the sum of the digits.
