59541cb1e01c24fefe606594b9748a7ff97bd05d
Nino Kharazishvili <ninokharazishvili@credo.ge>
2024-10-29 17:20:48 +0400

    minor changes

---
+++ homework11/index.js
@@ -0,0 +1,10 @@
+// 1) Fetch data from this API: https://jsonplaceholder.typicode.com/users.
+// Parse the data so that each object contains only four properties: id, name, username, and email.
+// Write the resulting array to a file called users.json.
+
+// 2) Run the command node main.js Ferrari 2020 red, retrieve the data from process.argv,
+// and build a car object with the properties id, carModel, carColor, and carReleaseDate.
+// Append this object to cars.json. Each time you run this command, a new object should be added to cars.json, 
+// so if you run it five times, you should have five objects in the file.
+
+// 3) Write a random text into a file named text.txt. Then, read this file and count how many vowels are present.
