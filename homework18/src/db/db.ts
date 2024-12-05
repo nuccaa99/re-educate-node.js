4461168533845c5c7d0b2d7dfad0e1c62d9870a7
Nino Kharazishvili <ninokharazishvili@credo.ge>
2024-12-05 16:08:40 +0400

    fixed minor issue

--- homework18/src/db/db.ts
+++ homework18/src/db/db.ts
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectionToDb = async (): Promise<void> => {
   try {
     await mongoose.connect(process.env.MONGODB_URL as string);
-    console.log('connected to DB');
+    console.log("connected to DB");
   } catch (e) {
-    console.error('cannot connect to DB');
+    console.error("cannot connect to DB");
   }
 };
